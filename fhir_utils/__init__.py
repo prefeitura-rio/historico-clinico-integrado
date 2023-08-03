249bb07921d0c65b445acae3422598b9a6f63520
thiagotrabach <thiago.trabach@gmail.com>
2023-08-02 21:39:17 -0300

    api access method changed

--- fhir_utils/__init__.py
+++ fhir_utils/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 from .conf import settings  # noqa
 from .version import __version__  # noqa
-from healthcare_api import HealthcareApi
+from fhir_utils.healthcare_api import HealthcareApi
--- fhir_utils/healthcare_api.py
+++ fhir_utils/healthcare_api.py
@@ -1,59 +1,68 @@
-from googleapiclient import discovery
-import json
+from google.auth.transport import requests
+from google.oauth2 import service_account
+import os
 
 class HealthcareApi:
-    def __init__(self, project_id, location, dataset_id, fhir_store_id):
-        self.api_version = "v1"
-        self.service_name = "healthcare"
-        self.header = "application/fhir+json;charset=utf-8"
-        self.fhir_store_parent = f"projects/{project_id}/locations/{location}/datasets/{dataset_id}"
-        self.fhir_store_name = f"{self.fhir_store_parent}/fhirStores/{fhir_store_id}"
-        self.client = discovery.build(self.service_name, self.api_version)
+    def __init__(self, project_id, location):
+        self.credentials = service_account.Credentials.from_service_account_file(os.environ["GOOGLE_APPLICATION_CREDENTIALS"])
+        self.scoped_credentials = self.credentials.with_scopes(["https://www.googleapis.com/auth/cloud-platform"])
+        self.session = requests.AuthorizedSession(self.scoped_credentials)
+        self.base_url = "https://healthcare.googleapis.com/v1"
+        self.url = f"{self.base_url}/projects/{project_id}/locations/{location}"
+        self.header = {"Content-Type": "application/fhir+json;charset=utf-8"}
 
 
-    def create(self, resource: str, body: dict):
-        request = self.client.projects().locations().datasets().fhirStores().fhir().create(parent = self.fhir_store_name,
-                                                                                           type = resource,
-                                                                                           body = body)
-        request.headers["content-type"] = self.header
+    def create(self, dataset_id: str, fhir_store_id: str, resource: str, payload: dict) -> None:
 
-        response = request.execute()
-        print(f"Created Patient resource with ID {response['id']}")
+        resource_path = f"{self.url}/datasets/{dataset_id}/fhirStores/{fhir_store_id}/fhir/{resource}"
 
-        return response
+        response = self.session.post(resource_path, headers=self.header, json=payload)
+        response.raise_for_status()
+
+        return_payload = response.json()
+
+        print(f"Created Patient resource with ID {return_payload['id']}")
+
+        return {'response': response, 'payload': return_payload}
 
 
-    def update(self, resource: str, resource_id: str, body: dict):
-        fhir_resource_path = f"{self.fhir_store_name}/fhir/{resource}/{resource_id}"
+    def update(self, dataset_id: str, fhir_store_id: str, resource: str, resource_id: str, payload: dict) -> dict:
 
-        request = self.client.projects().locations().datasets().fhirStores().fhir().update(name = fhir_resource_path,
-                                                                                           body = body)
+        resource_path = f"{self.url}/datasets/{dataset_id}/fhirStores/{fhir_store_id}/fhir/{resource}/{resource_id}"
 
-        request.headers["content-type"] = self.header
+        response = self.session.put(resource_path, headers=self.header, json=payload)
+        response.raise_for_status()
+
+        return_payload = response.json()
+
+        print(f"Updated {resource} resource with ID {resource_id}")    
+
+        return {'response': response, 'payload': return_payload}
+
+    
+    def read(self, dataset_id: str, fhir_store_id: str, resource: str, resource_id: str) -> dict:
+
+        resource_path = f"{self.url}/datasets/{dataset_id}/fhirStores/{fhir_store_id}/fhir/{resource}/{resource_id}"
         
-        response = request.execute()
-        print(f"Updated {resource} resource with ID {resource_id}:\n {json.dumps(response, indent=2)}")
+        response = self.session.get(resource_path, headers=self.header)
+        response.raise_for_status()
 
-        return response
+        return_payload = response.json()
 
-    
-    def read(self, resource: str, resource_id: str):
-        fhir_resource_path = f"{self.fhir_store_name}/fhir/{resource}/{resource_id}"
+        # remove xml from json payload
 
-        request = self.client.projects().locations().datasets().fhirStores().fhir().read(name = fhir_resource_path)
+        print(f"Got contents of {resource} resource with ID {resource_id}:\n")
 
-        response = request.execute()
-        print(f"Got contents of {resource} resource with ID {resource_id}:\n", json.dumps(response, indent=2),)
-
-        return response
+        return {'response': response, 'payload': return_payload}
 
     
-    def delete(self, resource: str, resource_id: str):
-        fhir_resource_path = f"{self.fhir_store_name}/fhir/{resource}/{resource_id}"
+    def delete(self, dataset_id: str, fhir_store_id: str, resource: str, resource_id: str) -> None:
+  
+        resource_path = f"{self.url}/datasets/{dataset_id}/fhirStores/{fhir_store_id}/fhir/{resource}/{resource_id}"
+        
+        response = self.session.delete(resource_path, headers=self.header)
+        response.raise_for_status()
 
-        request = self.client.projects().locations().datasets().fhirStores().fhir().delete(name = fhir_resource_path)
-
-        response = request.execute()
         print(f"Deleted {resource} resource with ID {resource_id}.")
 
-        return response
+        return {'response': response}
