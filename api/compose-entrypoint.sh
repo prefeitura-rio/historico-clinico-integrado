a0ae20c7fd7022008cae8837dca72d27aecee779
Pedro Nascimento <pedrovitor.marques@gmail.com>
2024-01-12 17:33:13 -0300

    feat: smart dockerfile with db initialization

--- api/compose-entrypoint.sh
+++ api/compose-entrypoint.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
+# Run aerich init-db if migrations folder does not exist
+if [ ! -d "./migrations/app/" ]; then
+    echo "Migrations folder does not exist, ASSUMING FIRST RUN"
 
-# Run aerich init-db
-aerich init-db
+    echo "Running aerich init-db to create initial migration"
+    aerich init-db || true
+
+    echo "Creating first user"
+    poetry run python create_user.py --username pedro --password senha --admin True
+
+    echo "Initializing Database Data"
+    poetry run python database_initial_data.py
+else
+    echo "./migrations/app/ folder exist, skipping initialization"
+fi
 
 # Run migrations
 aerich upgrade
