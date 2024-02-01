4cc1f9c3f5e6750460774780ecb8dd607437bd43
Pedro Nascimento <pedrovitor.marques@gmail.com>
2024-02-01 09:39:51 -0300

    filter raw data using datetime

--- api/app/routers/entities_raw.py
+++ api/app/routers/entities_raw.py
@@ -5,11 +5,10 @@ from typing import Annotated
 from fastapi import APIRouter, Depends
 from fastapi.responses import HTMLResponse
 
-import tortoise.timezone
 from tortoise.contrib.pydantic import pydantic_model_creator
 from tortoise.exceptions import ValidationError
 
-from app.pydantic_models import ( RawDataListModel, BulkInsertOutputModel )
+from app.pydantic_models import (RawDataListModel, BulkInsertOutputModel)
 from app.dependencies import get_current_active_user
 from app.models import (
     User, RawPatientRecord, RawPatientCondition, DataSource
@@ -21,7 +20,7 @@ router = APIRouter(prefix="/raw", tags=["Entidades RAW (Formato Raw/Bruto)"])
 
 RawPatientConditionInput = pydantic_model_creator(
     RawPatientCondition, name="RawPatientConditionInput",
-    exclude=("id","created_at","updated_at")
+    exclude=("id", "created_at", "updated_at")
 )
 RawPatientConditionOutput = pydantic_model_creator(
     RawPatientCondition, name="RawPatientConditionOutput"
@@ -29,7 +28,7 @@ RawPatientConditionOutput = pydantic_model_creator(
 
 RawPatientRecordInput = pydantic_model_creator(
     RawPatientRecord, name="RawPatientRecordInput",
-    exclude=("id","created_at","updated_at")
+    exclude=("id", "created_at", "updated_at")
 )
 RawPatientRecordOutput = pydantic_model_creator(
     RawPatientRecord, name="RawPatientRecordOutput"
@@ -38,15 +37,16 @@ RawPatientRecordOutput = pydantic_model_creator(
 
 @router.get("/patientrecords")
 async def get_raw_patientrecords(
-    _                   : Annotated[User, Depends(get_current_active_user)],
-    start_date          : datetime.date = tortoise.timezone.now().date(),
-    end_date            : datetime.date = tortoise.timezone.now().date() + datetime.timedelta(days=1),
-    datasource_system   : str = None,
+    _: Annotated[User, Depends(get_current_active_user)],
+    start_datetime: datetime.datetime = datetime.datetime.now() -
+    datetime.timedelta(hours=1),
+    end_datetime: datetime.datetime = datetime.datetime.now(),
+    datasource_system: str = None,
 ) -> list[RawPatientRecordOutput]:
 
     filtered = RawPatientRecord.filter(
-        created_at__gte=start_date,
-        created_at__lt=end_date
+        created_at__gte=start_datetime,
+        created_at__lt=end_datetime
     )
 
     if datasource_system is not None:
@@ -58,25 +58,25 @@ async def get_raw_patientrecords(
 
 @router.post("/patientrecords", status_code=201)
 async def create_raw_patientrecords(
-    _           : Annotated[User, Depends(get_current_active_user)],
-    raw_data    : RawDataListModel,
+    _: Annotated[User, Depends(get_current_active_user)],
+    raw_data: RawDataListModel,
 ) -> BulkInsertOutputModel:
 
     raw_data = raw_data.dict()
 
-    cnes        = raw_data.pop("cnes")
-    records     = raw_data.pop("data_list")
+    cnes = raw_data.pop("cnes")
+    records = raw_data.pop("data_list")
 
     try:
         records_to_create = []
         for record in records:
-                records_to_create.append(
-                    RawPatientRecord(
-                        patient_cpf     = record.get('patient_cpf'),
-                        data            = record.get('data'),
-                        data_source     = await DataSource.get(cnes=cnes)
-                    )
+            records_to_create.append(
+                RawPatientRecord(
+                    patient_cpf=record.get('patient_cpf'),
+                    data=record.get('data'),
+                    data_source=await DataSource.get(cnes=cnes)
                 )
+            )
     except ValidationError as e:
         return HTMLResponse(status_code=400, content=str(e))
 
@@ -88,15 +88,16 @@ async def create_raw_patientrecords(
 
 @router.get("/patientconditions")
 async def get_raw_patientconditions(
-    _                   : Annotated[User, Depends(get_current_active_user)],
-    start_date          : datetime.date = datetime.date.today(),
-    end_date            : datetime.date = datetime.date.today() + datetime.timedelta(days=1),
-    datasource_system   : str = None,
+    _: Annotated[User, Depends(get_current_active_user)],
+    start_datetime: datetime.datetime = datetime.datetime.now() -
+    datetime.timedelta(hours=1),
+    end_datetime: datetime.datetime = datetime.datetime.now(),
+    datasource_system: str = None,
 ) -> list[RawPatientConditionOutput]:
 
     filtered = RawPatientCondition.filter(
-        created_at__gte=start_date,
-        created_at__lt=end_date
+        created_at__gte=start_datetime,
+        created_at__lt=end_datetime
     )
 
     if datasource_system is not None:
@@ -108,24 +109,24 @@ async def get_raw_patientconditions(
 
 @router.post("/patientconditions", status_code=201)
 async def create_raw_patientconditions(
-    _           : Annotated[User, Depends(get_current_active_user)],
-    raw_data    : RawDataListModel,
+    _: Annotated[User, Depends(get_current_active_user)],
+    raw_data: RawDataListModel,
 ) -> BulkInsertOutputModel:
     raw_data = raw_data.dict()
 
-    cnes        = raw_data.pop("cnes")
-    conditions  = raw_data.pop("data_list")
+    cnes = raw_data.pop("cnes")
+    conditions = raw_data.pop("data_list")
 
     try:
         conditions_to_create = []
         for condition in conditions:
-                conditions_to_create.append(
-                    RawPatientCondition(
-                        patient_cpf     = condition.get('patient_cpf'),
-                        data            = condition.get('data'),
-                        data_source     = await DataSource.get(cnes=cnes)
-                    )
+            conditions_to_create.append(
+                RawPatientCondition(
+                    patient_cpf=condition.get('patient_cpf'),
+                    data=condition.get('data'),
+                    data_source=await DataSource.get(cnes=cnes)
                 )
+            )
     except ValidationError as e:
         return HTMLResponse(status_code=400, content=str(e))
 
