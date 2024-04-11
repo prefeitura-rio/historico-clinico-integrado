a371525bbb83f2ba2621a3e02decb320f135a020
Pedro Marques <pedrovitor.marques@gmail.com>
2024-04-11 12:36:09 -0300

    Adding Raw Source Index for Patient Record

--- api/app/models.py
+++ api/app/models.py
@@ -60,7 +60,7 @@ class StandardizedPatientRecord(Model):
     race                = fields.CharEnumField(enum_type=RaceEnum, null=True)
     gender              = fields.CharEnumField(enum_type=GenderEnum)
     nationality         = fields.CharEnumField(enum_type=NationalityEnum, null=True)
-    raw_source          = fields.ForeignKeyField("app.RawPatientRecord", related_name="std_record_raw", null=False)
+    raw_source          = fields.ForeignKeyField("app.RawPatientRecord", related_name="std_record_raw", null=False, index=True)
     cns_list            = fields.JSONField(null=True)
     address_list        = fields.JSONField(null=True)
     telecom_list        = fields.JSONField(null=True)
@@ -82,7 +82,7 @@ class StandardizedPatientCondition(Model):
     clinical_status     = fields.CharEnumField(enum_type=ClinicalStatusEnum, null=True)
     category            = fields.CharEnumField(enum_type=CategoryEnum, null=True)
     date                = fields.DatetimeField()
-    raw_source          = fields.ForeignKeyField("app.RawPatientCondition", related_name="std_condition_raw", null=False)
+    raw_source          = fields.ForeignKeyField("app.RawPatientCondition", related_name="std_condition_raw", null=False, index=False)
     is_valid            = fields.BooleanField(null=True)
 
     created_at          = fields.DatetimeField(auto_now_add=True)
---
+++ api/migrations/app/8_20240411123536_update.py
@@ -0,0 +1,11 @@
+from tortoise import BaseDBAsyncClient
+
+
+async def upgrade(db: BaseDBAsyncClient) -> str:
+    return """
+        CREATE INDEX "idx_std__patien_raw_sou_8d660c" ON "std__patientrecord" ("raw_source_id");"""
+
+
+async def downgrade(db: BaseDBAsyncClient) -> str:
+    return """
+        DROP INDEX "idx_std__patien_raw_sou_8d660c";"""
