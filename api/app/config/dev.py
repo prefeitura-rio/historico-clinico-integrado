da360ac09e9e4fd03de56ab762d5af59250b9ebf
Pedro Marques <pedrovitor.marques@gmail.com>
2024-06-25 09:50:51 -0300

    allow API to use the dev db outside container

--- api/app/config/dev.py
+++ api/app/config/dev.py
@@ -3,13 +3,24 @@ from . import getenv_list_or_action, getenv_or_action
 from .base import *  # noqa: F401, F403
 
 
+# ======================
+# DATABASE
+# ======================
 DATABASE_HOST = getenv_or_action("DATABASE_HOST", default="localhost")
 DATABASE_PORT = getenv_or_action("DATABASE_PORT", default="5432")
 DATABASE_USER = getenv_or_action("DATABASE_USER", default="postgres")
 DATABASE_PASSWORD = getenv_or_action("DATABASE_PASSWORD", default="postgres")
 DATABASE_NAME = getenv_or_action("DATABASE_NAME", default="postgres")
 
-# CORS configuration
+# Allow to run API to use the development db from outside container
+IN_DEBUGGER = getenv_or_action("IN_DEBUGGER", default="false").lower() == "true"
+if IN_DEBUGGER and DATABASE_HOST == "db":
+    print("Running in debugger mode, changing DATABASE_HOST to localhost")
+    DATABASE_HOST = "localhost"
+
+# ======================
+# CORS
+# ======================
 ALLOWED_ORIGINS = getenv_list_or_action("ALLOWED_ORIGINS", default=["*"])
 ALLOWED_ORIGINS_REGEX = None
 ALLOWED_METHODS = getenv_list_or_action("ALLOWED_METHODS", default=["*"])
