6c638d086506deea0b92ce839fa5ca5291f5bd8f
Pedro Nascimento <pedrovitor.marques@gmail.com>
2024-07-22 10:50:53 -0300

    chore: Including basedosdados and upload_to_datalake function

--- api/app/utils.py
+++ api/app/utils.py
@@ -3,7 +3,10 @@ from datetime import datetime, timedelta
 import hashlib
 import json
 import jwt
+import basedosdados as bd
+
 from passlib.context import CryptContext
+from loguru import logger
 
 from app import config
 from app.models import User
@@ -88,7 +91,8 @@ def generate_dictionary_fingerprint(dict_obj: dict) -> str:
 
     """
     serialized_obj = json.dumps(dict_obj, sort_keys=True)
-    return hashlib.md5(serialized_obj.encode('utf-8')).hexdigest()
+    return hashlib.md5(serialized_obj.encode("utf-8")).hexdigest()
+
 
 def merge_versions(current_objs, new_objs: dict) -> None:
     current_fingerprints = {obj.fingerprint: obj for obj in current_objs}
@@ -97,20 +101,16 @@ def merge_versions(current_objs, new_objs: dict) -> None:
     to_delete = current_fingerprints.keys() - new_fingerprints.keys()
     to_add = new_fingerprints.keys() - current_fingerprints.keys()
 
-    deletions = [
-        current_fingerprints[fingerprint]
-        for fingerprint in to_delete
-    ]
-    insertions = [
-        new_fingerprints[fingerprint]
-        for fingerprint in to_add
-    ]
+    deletions = [current_fingerprints[fingerprint] for fingerprint in to_delete]
+    insertions = [new_fingerprints[fingerprint] for fingerprint in to_add]
 
     return deletions, insertions
+
 
 async def update_and_return(instance, new_data):
     await instance.update_from_dict(new_data).save()
     return instance
+
 
 async def get_instance(Model, table, slug=None, code=None):
     if slug is None:
@@ -123,3 +123,102 @@ async def get_instance(Model, table, slug=None, code=None):
             table[slug] = await Model.get_or_none(slug=slug)
 
     return table[slug]
+
+
+def upload_to_datalake(
+    input_path: str,
+    dataset_id: str,
+    table_id: str,
+    dump_mode: str = "append",
+    source_format: str = "csv",
+    csv_delimiter: str = ";",
+    if_exists: str = "replace",
+    if_storage_data_exists: str = "replace",
+    biglake_table: bool = True,
+    dataset_is_public: bool = False,
+):
+    """
+    Uploads data to a Google Cloud Storage bucket and creates or appends to a BigQuery table.
+
+    Args:
+        input_path (str): The path to the input data file.
+        dataset_id (str): The ID of the BigQuery dataset.
+        table_id (str): The ID of the BigQuery table.
+        dump_mode (str, optional): The dump mode for the table. Defaults to "append". Accepted values are "append" and "overwrite".
+        source_format (str, optional): The format of the input data. Defaults to "csv". Accepted values are "csv" and "parquet".
+        csv_delimiter (str, optional): The delimiter used in the CSV file. Defaults to ";".
+        if_exists (str, optional): The behavior if the table already exists. Defaults to "replace".
+        if_storage_data_exists (str, optional): The behavior if the storage data already exists. Defaults to "replace".
+        biglake_table (bool, optional): Whether the table is a BigLake table. Defaults to True.
+        dataset_is_public (bool, optional): Whether the dataset is public. Defaults to False.
+
+    Raises:
+        RuntimeError: If an error occurs during the upload process.
+
+    Returns:
+        None
+    """
+    if input_path == "":
+        logger.warning("Received input_path=''. No data to upload")
+        return
+
+    tb = bd.Table(dataset_id=dataset_id, table_id=table_id)
+    table_staging = f"{tb.table_full_name['staging']}"
+    st = bd.Storage(dataset_id=dataset_id, table_id=table_id)
+    storage_path = f"{st.bucket_name}.staging.{dataset_id}.{table_id}"
+    storage_path_link = (
+        f"https://console.cloud.google.com/storage/browser/{st.bucket_name}"
+        f"/staging/{dataset_id}/{table_id}"
+    )
+    logger.info(f"Uploading file {input_path} to {storage_path} with {source_format} format")
+
+    try:
+        table_exists = tb.table_exists(mode="staging")
+
+        if not table_exists:
+            logger.info(f"CREATING TABLE: {dataset_id}.{table_id}")
+            tb.create(
+                path=input_path,
+                source_format=source_format,
+                csv_delimiter=csv_delimiter,
+                if_storage_data_exists=if_storage_data_exists,
+                biglake_table=biglake_table,
+                dataset_is_public=dataset_is_public,
+            )
+        else:
+            if dump_mode == "append":
+                logger.info(
+                    f"TABLE ALREADY EXISTS APPENDING DATA TO STORAGE: {dataset_id}.{table_id}"
+                )
+
+                tb.append(filepath=input_path, if_exists=if_exists)
+            elif dump_mode == "overwrite":
+                logger.info(
+                    "MODE OVERWRITE: Table ALREADY EXISTS, DELETING OLD DATA!\n"
+                    f"{storage_path}\n"
+                    f"{storage_path_link}"
+                )  # pylint: disable=C0301
+                st.delete_table(mode="staging", bucket_name=st.bucket_name, not_found_ok=True)
+                logger.info(
+                    "MODE OVERWRITE: Sucessfully DELETED OLD DATA from Storage:\n"
+                    f"{storage_path}\n"
+                    f"{storage_path_link}"
+                )  # pylint: disable=C0301
+                tb.delete(mode="all")
+                logger.info(
+                    "MODE OVERWRITE: Sucessfully DELETED TABLE:\n" f"{table_staging}\n"
+                )  # pylint: disable=C0301
+
+                tb.create(
+                    path=input_path,
+                    source_format=source_format,
+                    csv_delimiter=csv_delimiter,
+                    if_storage_data_exists=if_storage_data_exists,
+                    biglake_table=biglake_table,
+                    dataset_is_public=dataset_is_public,
+                )
+        logger.info("Data uploaded to BigQuery")
+
+    except Exception as e:  # pylint: disable=W0703
+        logger.error(f"An error occurred: {e}", level="error")
+        raise RuntimeError() from e
