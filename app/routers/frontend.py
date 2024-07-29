dd97b7d98b4c92753dacc152592425d74f0c6607
Pedro Nascimento <pedrovitor.marques@gmail.com>
2024-07-29 18:44:50 -0300

    chore: Add frontend router to main.py and update frontend endpoints

--- app/routers/frontend.py
+++ app/routers/frontend.py
@@ -2,63 +2,70 @@
 from typing import Annotated, List
 
 from fastapi import APIRouter, Depends
-from fastapi.responses import HTMLResponse
-
-from tortoise.contrib.pydantic import pydantic_model_creator
-from tortoise.exceptions import ValidationError
 
 from app.dependencies import get_current_active_user
-from app.pydantic_models import (
-    MergedPatient as PydanticMergedPatient,
-)
 from app.models import User
+from app.types.frontend import (
+    PatientHeader,
+    FamilyClinic,
+    FamilyHealthTeam,
+    Responsible,
+    PatientSummary,
+    Encounter,
+    UserInfo,
+)
 
 
-router = APIRouter(prefix="/frontend", tags=["Endpoints para consumo da aplicação Frontend"])
+router = APIRouter(prefix="/frontend", tags=["Frontend Application"])
 
 
 @router.get("/user")
-async def get_patient_header(
+async def get_user_info(
     user: Annotated[User, Depends(get_current_active_user)],
-) -> dict:
-    cpf = user.cpf
+) -> UserInfo:
+    if user.cpf:
+        cpf = user.cpf
+        cpf = f"{cpf[:3]}.{cpf[3:6]}.{cpf[6:9]}-{cpf[9:]}"
+    else:
+        cpf = None
 
     return {
-        "nome": user.name,
+        "name": user.name,
         "email": user.email,
-        "CPF": f"{cpf[:3]}.{cpf[3:6]}.{cpf[6:9]}-{cpf[9:]}",
+        "username": user.username,
+        "cpf": cpf,
     }
 
 
 @router.get("/patient/header/{cpf}")
 async def get_patient_header(
     _: Annotated[User, Depends(get_current_active_user)],
-    cpf: int,
-) -> dict:
+    cpf: str,
+) -> PatientHeader:
     return {
-        "nome_registro": "José da Silva Xavier",
-        "nome_social": None,
+        "registration_name": "José da Silva Xavier",
+        "social_name": None,
         "cpf": f"{cpf[:3]}.{cpf[3:6]}.{cpf[6:9]}-{cpf[9:]}",
         "cns": "123456789012345",
-        "data_nascimento": "1972-08-01",
-        "genero": "masculino",
-        "raca": "parda",
-        "telefone": "(21) 99999-9999",
-        "clinica_familia": {"cnes": "1234567", "nome": "Clinica da Familia XXX"},
-        "equipe_saude_familia": {"id_ine": "1234567", "nome": "Equipe Roxo"},
-        "responsavel_medico": "Roberta dos Santos",
-        "responsavel_enfermagem": "Pedro da Nobrega",
-        "cadastro_validado_indicador": True,
+        "birth_date": "1972-08-01",
+        "gender": "masculino",
+        "race": "parda",
+        "phone": "(21) 99999-9999",
+        "family_clinic": {"cnes": "1234567", "name": "Clinica da Familia XXX"},
+        "family_health_team": {"ine_code": "1234567", "name": "Equipe Roxo"},
+        "medical_responsible": "Roberta dos Santos",
+        "nursing_responsible": "Pedro da Nobrega",
+        "validated": True,
     }
 
 
 @router.get("/patient/summary/{cpf}")
 async def get_patient_summary(
     _: Annotated[User, Depends(get_current_active_user)],
-    cpf: int,
-) -> list:
+    cpf: str,
+) -> PatientSummary:
     return {
-        "alergias": [
+        "allergies": [
             "Sulfonamidas",
             "Ácaros do pó",
             "Penicilina",
@@ -70,7 +77,7 @@ async def get_patient_summary(
             "Preservativos",
             "Luvas de látex",
         ],
-        "medicamentos_uso_continuo": [
+        "continuous_use_medications": [
             "Losartana potássica",
             "Enalapril maleato",
             "Besilato de anlodipino",
@@ -82,31 +89,36 @@ async def get_patient_summary(
 
 
 @router.get("/patient/encounters/{cpf}")
-async def get_patient_encouters(
+async def get_patient_encounters(
     _: Annotated[User, Depends(get_current_active_user)],
-    cpf: int,
-) -> list:
+    cpf: str,
+) -> List[Encounter]:
     return [
         {
-            "datahora_entrada": "2023-09-01T10:00:00",
-            "datahora_saida": "2023-09-01T12:00:00",
-            "local": "UPA 24h Magalhães Bastos",
-            "tipo": "Consulta",
-            "subtipo": "Emergência",
-            "cids_ativos": ["A10.2", "B02.5"],
-            "responsavel": {"nome": "Dr. João da Silva", "funcao": "Médico(a)"},
-            "descricao": "Lorem ipsum dolor sit amet consectetur. ",
-            "tags_filtro": ["UPA"],
+            "entry_datetime": "2023-09-01T10:00:00",
+            "exit_datetime": "2023-09-01T12:00:00",
+            "location": "UPA 24h Magalhães Bastos",
+            "type": "Consulta",
+            "subtype": "Emergência",
+            "active_cids": ["A10.2", "B02.5"],
+            "responsible": {"name": "Dr. João da Silva", "role": "Médico(a)"},
+            "description": "Lorem ipsum dolor sit amet consectetur.",
+            "filter_tags": ["UPA"],
         },
         {
-            "datahora_entrada": "2021-09-01T10:00:00",
-            "datahora_saida": "2021-09-01T12:00:00",
-            "local": "UPA 24h Magalhães Bastos",
-            "tipo": "Consulta",
-            "subtipo": "Emergência",
-            "cids_ativos": ["A10.2"],
-            "responsavel": {"nome": "Dr. João da Silva", "funcao": "Médico(a)"},
-            "descricao": "Lorem ipsum dolor sit amet consectetur. Sed vel suscipit id pulvinar sed nam libero eu. Leo arcu sit lacus nisl nullam eget et dignissim sed. Fames pretium cursus viverra posuere arcu tortor sit lectus congue. Velit tempor ultricies pulvinar magna pulvinar ridiculus consequat nibh...",
-            "tags_filtro": ["UPA", "Emergência"],
+            "entry_datetime": "2021-09-01T10:00:00",
+            "exit_datetime": "2021-09-01T12:00:00",
+            "location": "UPA 24h Magalhães Bastos",
+            "type": "Consulta",
+            "subtype": "Emergência",
+            "active_cids": ["A10.2"],
+            "responsible": {"name": "Dr. João da Silva", "role": "Médico(a)"},
+            "description": (
+                "Lorem ipsum dolor sit amet consectetur. Sed vel suscipit id pulvinar sed nam libero eu."
+                "Leo arcu sit lacus nisl nullam eget et dignissim sed. Fames pretium cursus viverra "
+                "posuere arcu tortor sit lectus congue. Velit tempor ultricies pulvinar magna pulvinar "
+                "ridiculus consequat nibh..."
+            ),
+            "filter_tags": ["UPA", "Emergência"],
         },
     ]
