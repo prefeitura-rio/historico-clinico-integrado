59fe78a1dfbc55ef65fa0b4ccb24d449ba6a41e8
Pedro Nascimento <pedrovitor.marques@gmail.com>
2024-09-24 16:55:16 -0300

    fix: script for dropping deprecated tables

--- migrations/app/33_20240924164833_update.py
+++ migrations/app/33_20240924164833_update.py
@@ -4,30 +4,30 @@ from tortoise import BaseDBAsyncClient
 
 async def upgrade(db: BaseDBAsyncClient) -> str:
     return """
-        DROP TABLE IF EXISTS "city";
-        DROP TABLE IF EXISTS "race";
-        DROP TABLE IF EXISTS "state";
-        DROP TABLE IF EXISTS "gender";
-        DROP TABLE IF EXISTS "country";
-        DROP TABLE IF EXISTS "occupation";
-        DROP TABLE IF EXISTS "nationality";
-        DROP TABLE IF EXISTS "raw__encounter";
-        DROP TABLE IF EXISTS "conditioncode";
-        DROP TABLE IF EXISTS "mrg__patient";
-        DROP TABLE IF EXISTS "healthcareteam";
-        DROP TABLE IF EXISTS "mrg__patientcns";
-        DROP TABLE IF EXISTS "occupationfamily";
-        DROP TABLE IF EXISTS "raw__patientrecord";
-        DROP TABLE IF EXISTS "healthcareteamtype";
-        DROP TABLE IF EXISTS "raw__patientcondition";
-        DROP TABLE IF EXISTS "mrg__patientaddress";
-        DROP TABLE IF EXISTS "mrg__patienttelecom";
-        DROP TABLE IF EXISTS "professionalregistry";
-        DROP TABLE IF EXISTS "healthcareprofessional";
-        DROP TABLE IF EXISTS "std__patientrecord";
-        DROP TABLE IF EXISTS "healthcareprofessional_team";
-        DROP TABLE IF EXISTS "std__patientcondition";
-        DROP TABLE IF EXISTS "healthcareprofessional_occupation";"""
+        DROP TABLE IF EXISTS "city" CASCADE;
+        DROP TABLE IF EXISTS "race" CASCADE;
+        DROP TABLE IF EXISTS "state" CASCADE;
+        DROP TABLE IF EXISTS "gender" CASCADE;
+        DROP TABLE IF EXISTS "country" CASCADE;
+        DROP TABLE IF EXISTS "occupation" CASCADE;
+        DROP TABLE IF EXISTS "nationality" CASCADE;
+        DROP TABLE IF EXISTS "raw__encounter" CASCADE;
+        DROP TABLE IF EXISTS "conditioncode" CASCADE;
+        DROP TABLE IF EXISTS "mrg__patient" CASCADE;
+        DROP TABLE IF EXISTS "healthcareteam" CASCADE;
+        DROP TABLE IF EXISTS "mrg__patientcns" CASCADE;
+        DROP TABLE IF EXISTS "occupationfamily" CASCADE;
+        DROP TABLE IF EXISTS "raw__patientrecord" CASCADE;
+        DROP TABLE IF EXISTS "healthcareteamtype" CASCADE;
+        DROP TABLE IF EXISTS "raw__patientcondition" CASCADE;
+        DROP TABLE IF EXISTS "mrg__patientaddress" CASCADE;
+        DROP TABLE IF EXISTS "mrg__patienttelecom" CASCADE;
+        DROP TABLE IF EXISTS "professionalregistry" CASCADE;
+        DROP TABLE IF EXISTS "healthcareprofessional" CASCADE;
+        DROP TABLE IF EXISTS "std__patientrecord" CASCADE;
+        DROP TABLE IF EXISTS "healthcareprofessional_team" CASCADE;
+        DROP TABLE IF EXISTS "std__patientcondition" CASCADE;
+        DROP TABLE IF EXISTS "healthcareprofessional_occupation" CASCADE;"""
 
 
 async def downgrade(db: BaseDBAsyncClient) -> str:
