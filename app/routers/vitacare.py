6f85460b9a077b85872308ad70aa872925dea057
Pedro <pedrovitor.marques@gmail.com>
2025-03-31 15:14:27 -0300

    fix: enhance error handling in load_data function to include timeout exception handling and improve response management

--- app/routers/vitacare.py
+++ app/routers/vitacare.py
@@ -39,41 +39,50 @@ async def load_data(
     elif entity_name == "encounter":
         entity_name = "atendimento"
 
-    async with httpx.AsyncClient() as client:
-        logger.info(f"Getting token from datalake hub...")
-        response = await client.post(
-            url=f"{config.DATALAKE_HUB_URL}auth/token",
-            headers={
-                "accept": "application/json",
-                "Content-Type": "application/x-www-form-urlencoded"
-            },
-            data={
-                "grant_type": "password",
-                "username": config.DATALAKE_HUB_USERNAME,
-                "password": config.DATALAKE_HUB_PASSWORD,
-            },
-            timeout=90
-        )
-
-        if response.status_code != 200:
-            return JSONResponse(
-                status_code=response.status_code,
-                content={
-                    "message": "Failed to get token from datalake hub",
-                    "content": response.text
-                }
+    try:
+        async with httpx.AsyncClient() as client:
+            logger.info(f"Getting token from datalake hub...")
+            response = await client.post(
+                url=f"{config.DATALAKE_HUB_URL}auth/token",
+                headers={
+                    "accept": "application/json",
+                    "Content-Type": "application/x-www-form-urlencoded"
+                },
+                data={
+                    "grant_type": "password",
+                    "username": config.DATALAKE_HUB_USERNAME,
+                    "password": config.DATALAKE_HUB_PASSWORD,
+                },
+                timeout=90
             )
 
-        token = response.json().get("access_token")
+            if response.status_code != 200:
+                return JSONResponse(
+                    status_code=response.status_code,
+                    content={
+                        "message": "Failed to get token from datalake hub",
+                        "content": response.text
+                    }
+                )
 
-        logger.info(f"Sending data to datalake hub...")
-        response = await client.post(
-            url=f"{config.DATALAKE_HUB_URL}vitacare/{entity_name}",
-            headers={
-                "Authorization": f"Bearer {token}"
-            },
-            json=json.loads(raw_data.json()),
-            timeout=90
+            token = response.json().get("access_token")
+
+            logger.info(f"Sending data to datalake hub...")
+            response = await client.post(
+                url=f"{config.DATALAKE_HUB_URL}vitacare/{entity_name}",
+                headers={
+                    "Authorization": f"Bearer {token}"
+                },
+                json=json.loads(raw_data.json()),
+                timeout=90
+            )
+    except httpx.TimeoutException as e:
+        return JSONResponse(
+            status_code=504,
+            content={
+                "message": "Timeout error",
+                "content": str(e)
+            }
         )
 
     return JSONResponse(
